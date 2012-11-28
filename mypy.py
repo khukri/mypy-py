5da0968a6414f0366eb0162f7808f156f2a4c46e
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2012-11-28 10:21:17 +0000

    Update generated code

--- mypy.py
+++ mypy.py
@@ -1,90 +1,123 @@
+"""Mypy type checker and Python translator
+
+Type check and program, translate it to Python and run it. Note that you must
+use a mypy translator that has already been translated to run this program.
+"""
+
 import os
 import os.path
 import sys
+import tempfile
+import shutil
 
 from build import build
 from pythongen import PythonGenerator
 from errors import CompileError
 
 
+# Fallback options
+verbose = False
+pyversion = 3
+interpreter = 'python'
+
+
 def main():
-    verbose = False
-    pyversion = 3
-    args = sys.argv[1:]
-    while args and args[0].startswith('-'):
-        if args[0] == '--verbose':
-            verbose = True
-            args = args[1:]
-        elif args[0] == '--py2':
-            pyversion = 2
-            args = args[1:]
-        else:
-            fail('Invalid option {}'.format(args[0]))
-    
-    if not args:
-        fail()
-    
-    path = args[0]
+    path, args = process_options()
     
     mainfile = open(path)
     text = mainfile.read()
     mainfile.close()
     
     try:
-        # TODO determine directory more intelligently
-        # TODO make sure only the current user can access the directory
-        output_dir = '/tmp/mypy-xx'
-        if not os.path.isdir(output_dir):
-            os.makedirs(output_dir, 0o777)
-        
-        # Parse and type check the program and dependencies.
-        trees, symtable, infos, types = build(text, path, False, None, True)
-        
-        # Translate each file in the program to Python.
-        # TODO support packages
-        for t in trees:
-            if not is_stub(t.path):
-                out_path = os.path.join(output_dir, os.path.basename(t.path))
-                if verbose:
-                    print('LOG: translate {} to {}'.format(t.path, out_path))
-                v = PythonGenerator(pyversion)
-                t.accept(v)
-                outfile = open(out_path, 'w')
-                outfile.write(v.output())
-                outfile.close()
-        
-        # Run the translated program.
-        # TODO determine path to Python interpreter reliably
-        
-        a = []
-        for arg in args[1:]:
-            # TODO escape arguments etc.
-            a.append('"{}"'.format(arg))
+        outputdir = os.path.join(os.path.dirname(path), '__mycache__')
+        tempdir = False
+        if not os.path.isdir(outputdir):
+            try:
+                os.mkdir(outputdir)
+            except OSError:
+                # Could not create a directory under program directory; must
+                # fall back to a temp directory. It will be removed later.
+                outputdir = tempfile.mkdtemp()
+                tempdir = True
 
-        cmd = 'python3'
-        if pyversion == 2:
-            cmd = 'python'
+        try:
+            # Parse and type check the program and dependencies.
+            trees, symtable, infos, types = build(text, path, False, None,
+                                                  True)
         
-        os.system('{} "{}/{}" {}'.format(
-                             cmd, output_dir, os.path.basename(path),
-                             ' '.join(a)))
+            # Translate each file in the program to Python.
+            # TODO support packages
+            for t in trees:
+                if not is_stub(t.path):
+                    out_path = os.path.join(outputdir,
+                                            os.path.basename(t.path))
+                    log('translate {} to {}'.format(t.path, out_path))
+                    v = PythonGenerator(pyversion)
+                    t.accept(v)
+                    outfile = open(out_path, 'w')
+                    outfile.write(v.output())
+                    outfile.close()
+
+            # Run the translated program.
+
+            a = []
+            for arg in args[1:]:
+                # TODO escape arguments etc.
+                a.append('"{}"'.format(arg))
+
+            os.system('{} "{}/{}" {}'.format(interpreter, outputdir,
+                                             os.path.basename(path),
+                                             ' '.join(a)))
+        finally:
+            if tempdir:
+                shutil.rmtree(outputdir)
     except CompileError as e:
         for m in e.messages:
             print(m)
         sys.exit(2)
 
 
-def fail(msg=None):
+def process_options():
+    if sys.executable:
+        global interpreter
+        interpreter = sys.executable
+    args = sys.argv[1:]
+    while args and args[0].startswith('-'):
+        if args[0] == '--verbose':
+            global verbose
+            verbose = True
+            args = args[1:]
+        elif args[0] == '--py2' and args[1:]:
+            # Generate Python 2 (but this is very buggy).
+            global pyversion
+            pyversion = 2
+            interpreter = args[1]
+            args = args[2:]
+        else:
+            fail('Invalid option {}'.format(args[0]))
+    
+    if not args:
+        fail()
+    
+    return args[0], args[1:]    
+
+
+def fail( msg=None):
     if msg:
         sys.stderr.write('%s\n' % msg)
     sys.stderr.write('Usage: mypy.py [--verbose] PROGRAM\n')
     sys.exit(1)
 
 
-def is_stub(path):
+def is_stub( path):
     # TODO make the check more precise
     return path.startswith('stubs/') or '/stubs/' in path
 
 
-# TODO if __name__ == '__main__'
-main()
+def log( message):
+    if verbose:
+        print('LOG: {}'.format(message))
+
+
+if __name__ == '__main__':
+    main()
